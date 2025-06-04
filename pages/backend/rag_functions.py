b600593410a81e67a3c81d424ddbc4c3cae5b638
Nina Chekalina <126569742+ninachekalina@users.noreply.github.com>
2025-06-04 18:48:16 +0300

    Update rag_functions.py

--- pages/backend/rag_functions.py
+++ pages/backend/rag_functions.py
@@ -8,9 +8,9 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_core.prompts import PromptTemplate
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
-from langchain.chains import create_retrieval_chain, LLMChain
+from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
+#from langchain.chains import create_retrieval_chain
+from langchain.chains import  LLMChain
 from langchain_core.tools import tool
 from langchain.tools.retriever import create_retriever_tool
 from langchain.chat_models.gigachat import GigaChat
